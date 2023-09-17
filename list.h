7d0d591b143865ad221db729809deb8e08d14dae
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-09-17 11:43:58 -0700

    Update list.h

--- list.h
+++ list.h
@@ -11,37 +11,38 @@ private:
     Element * curr;                                                   // Points to current Element
     size_t    totalMemUsed;                                           // Make program self-aware of how much memory it's using - Adds 8 bytes and calculation upon List insert/remove to overhead 
 public:
-    ~List();                                                          // Destructor
-    void clear();                                                     // Deallocate memory occupied by the linked list 
+   ~List();                                                           // Destructor
     List();                                                           // Default constructor
     List(Element*);                                                   // Parameterized constructor
     List(const List &other);                                          // Copy constructor
     
-    int  size() const;                                                // Size Accessor
-
     void insert(      Element *, int);                                // Insert at position
+    void insert(const Element &, int);                                // Insert at position       <- Delegates to Existing ^
     bool insert(      Element *     );                                // Sorted Insertion Mutator
-    void insert(const Element &, int);                                // Insert at position
-    bool insert(const Element &     );                                // Sorted Insertion Mutator
+    bool insert(const Element &     );                                // Sorted Insertion Mutator <- Delegates to Existing ^
     
-    bool findMatch  (Element  );                                      // Find Exact Match
-    bool findMatch  (Element *);                                      // Find Exact Match
+    void clear();                                                     // Deallocate memory of the linked list - completely empty the list.
+    void remove(      int      );                                     // Remove at position
+    bool remove(      Element *);                                     // Remove by value
+    bool remove(const Element &);                                     // Remove by value <- Delegates to Existing ^ 
+    
+    bool findMatch(      Element *);                                  // Find Exact Match
+    bool findMatch(const Element &);                                  // Find Exact Match <- Delegates to Existing ^
 
-    bool containsSequence(const Element &);                           // Contains Chars in Sequence | e.g. car, aM, haM, caM, (etc.) contained in charM
-    bool containsSequence(Element *);                                 // Contains Chars in Sequence | e.g. car, aM, haM, caM, (etc.) contained in charM
-
-    bool wildcardSearch(Element  );                                   // Search String Starts with*, ends *with, or *contains*
-    bool wildcardSearch(Element *);                                   // Search String Starts with*, ends *with, or *contains*
+    bool containsSequence  (      Element *);                         // Contains Chars in Sequence | e.g. car, aM, haM, caM, (etc.) contained in charM
+    bool containsSequence  (const Element &);                         // Contains Chars in Sequence <- Delegates to Existing ^
+    bool containsSequence_i(      Element *);                         // Case insensitive contains  | e.g. car, am, ham, cam, HAM (etc.) contained in CHarM
+    bool containsSequence_i(const Element &);                         // Case insensitive contains  <- Delegates to Existing ^
+    
+    bool wildcardSearch(      Element *);                             // Search String Starts with*, ends *with, or *contains*
+    bool wildcardSearch(const Element &);                             // Search String Starts with*, ends *with, or *contains*
     bool wildcardSearch(const char *, const char*);                   // Wildcard Search string helper.
-    
-    void remove(int      );                                           // Remove at position
-    bool remove(Element *);                                           // Remove by value
-    bool remove(Element  );                                           // Remove by value
     
     void getDeets();                                                  // Display information about the MEMORY of the List  
     void print();                                                     // Display information about the List  
     void printReverse();                                              // Display information about the List in REVERSE
     
+    int  size() const;                                                // Size Accessor - Returns size of List
     int  sizeOfLargestElement() const;                                // Get size of largest element in the List
   
     inline size_t       getMemUsed()    const {return totalMemUsed;}; // Get Memory Size of this List  
