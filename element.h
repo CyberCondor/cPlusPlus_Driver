260cca334e6a4c723fab8ce093a28eb6a0af9079
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-10-20 16:26:01 -0700

    Update element.h
    Extend functionality, memory security, and overload operators.

--- element.h
+++ element.h
@@ -19,8 +19,11 @@ public:
     char & operator [](int);                                 // Reference Mutator Subscript Operator
     void strcpy(char*,const char*);                          // Used for Dynamic Copy Construction
 
-    Element *  setNext(Element *);                                 // Set pointer to next
-    
+    Element *  setNext(Element *);                           // Set pointer to next
+
+    void setData(const char*);  
+    void appendData(const char &);  
+
     inline const char    * getData() const {return data;};         // Data Accessor
     inline       char    * getData()       {return data;};         // Data Accessor
     inline const Element * getNext() const {return next;};         // Get Next
@@ -33,15 +36,20 @@ public:
   
     inline int    length()           {return strlen(this->data);}; // Length Accessor - excludes nullbyte in length
     inline size_t getMemUsed() const {return totalMemUsed;};       // Mem Size Accessor  
-
-    char * intToString(int);                                       // Custom Int to String function - used to append an int to a Element 'string'
-
-    Element   operator+ (const Element&) const;                    // Append and Element to Element 
+    
+    Element & operator+ (const Element&);                          // Append and Element to Element 
     Element   operator+ (const char*);                             // Append a char to Element
     Element   operator+ (      char );                             // Append a char to Element
     Element   operator+ (      int  );                             // Append an Int to Element
-    Element & operator+=(      int  );                             // Append an Int to Element
+    Element & operator+=(const int  );                             // Append an Int to Element
     Element & operator+=(const char*);                             // Append a char to Element
+    Element & operator+=(const char&);                             // Append a char to Element
+    Element & operator+=(const Element&);                          // Append a char to Element
+    
+    Element & operator= (const Element  &other);                   // Copy assignment operator
+    Element & operator= (      Element &&other);                   // Move assignment operator
+    
+    char * intToString(int);                                       // Custom Int to String function - used to append an int to a Element 'string'
 };
                                                               // Operator Overload Functions
 bool operator == (const Element &, const Element &);          // Equality                  Operator
@@ -53,8 +61,11 @@ bool operator <  (const Element &, const Element &);          // Less Than      
 
 std::ostream & operator << (std::ostream &, const Element &); // Stream Insertion
 std::istream & operator >> (std::istream &, Element &);       // Stream Extraction
+std::istream & operator >> (std::istream &, Element *);       // Stream Extraction
 
-int strcmp(const char *, const char *);                       // Standard Element Compare
+int strcmp  (const char *, const char *);                     // Standard Element Compare
+int strcmp_i(const char *, const char *);                     // Standard Element Compare - Case Insensitive.
 
-char toUpper(char);
-char toLower(char);
+char    toUpper(const char    &);
+char    toLower(const char    &);
+Element intToString(int);                                     // Custom Int to String function - used to append an int to a Element 'string'
