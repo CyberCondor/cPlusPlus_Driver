275b058ce74d86908023d862dec86de47a9e802e
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-10-29 16:36:42 -0700

    Update main.h

--- main.h
+++ main.h
@@ -2,8 +2,14 @@
 
 #include <iostream>
 #include "element.h"
+#include "string.h"
 #include "list.h"
 #include "matrix.h"
+#include "space.h"
 #include "object.h"
+#include "position.h"
+#include "positions.h"
+#include "property.h"
+#include "properties.h"
 
 int main(int, char **, char **);
