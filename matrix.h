4bd7d92769d85d714b166f3287c864862571c06d
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-10-29 16:14:19 -0700

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -3,32 +3,60 @@
 #include <iomanip>
 #include "list.h"
 #include "element.h"
-
+#include "positions.h"
+#include "position.h"
+// A matrix is also known as a 2D plane - it's just a plot of points or like an Excel spreadsheet.
 class Matrix {
 private:
-    List * rows;    // List of rows
-    List * columns; // List of columns
-    int    numRows; // Number of rows
-    int    numCols; // Number of columns
-
+    Positions * X;
+    Positions * Y;
+    List      * rows;    // List of rows
+    List      * columns; // List of columns
+    int         numRows; // Number of rows
+    int         numCols; // Number of columns
+    Position    position;// Position in 3D space
+    Matrix    * next;    // Next element to traverse 3D space
 public:
-    ~Matrix();                         // Destructor!
-    Matrix();                          // Default Constructor    
-    Matrix(int, int);                  // Constructor to create a matrix with the specified number of rows and columns
-    Matrix(int, int, const Element &); // Constructor to create a matrix with the specified number of rows and columns & a default Element
+    ~Matrix();                              // Destructor!
+    Matrix();                               // Default Constructor    
+    Matrix(const Matrix   &);               // Copy    Constructor    
+    Matrix(      Matrix   *);               // Param   Constructor    
+    Matrix(const Position &);               // Constructor to create a 1x1 matrix with z specified
+    Matrix(int);                            // Constructor to create a 1x1 matrix with z specified
+    Matrix(int, int);                       // Constructor to create a matrix with the specified number of rows and columns
+    Matrix(int, int, int);                  // Constructor to create a matrix with the specified number of rows and columns and z
+    Matrix(int, int,      const Element  &);// Constructor to create a matrix with the specified number of rows and columns & a default Element
+    Matrix(int, int, int, const Element  &);// Constructor to create a matrix with the specified number of rows and columns & a default Element
 
     void transpose();
 
     void resize(int, int);
 
-    Element getValue (int, int) const;
-    void    setValue (int, int, const Element &);
-    void    setValues(const Element &);
+    Element getValue  (int, int) const;
+    Element getValue_i(int, int) const;
+    void    setValue  (int, int, const Element &);
+    void    setValues (          const Element &);
 
-    void print() const;
+    void print()                   const;
+    void printPositions()          const;
+    void printEachPosition()       const;
 
-    void printRow   (int) const;       // Print a specific row
-    void printColumn(int) const;       // Print a specific column
+    void printRowPositions()       const;
+    void printColPositions()       const;
+
+    void printRowPositionsRaw()    const;
+    void printColPositionsRaw()    const;
+
+    void printPositionsCrawlLeft() const;
+    void printPositionsCrawlRight()const;
+
+    void printPositionsSlopeLeft() const;
+    void printPositionsSlopeRight()const;
+
+    void printRow    (int) const;       // Print a specific row    no new line
+    void printColumn (int) const;       // Print a specific column no new line
+    void prettyRow   (int) const;       // Print a specific row       new line
+    void prettyColumn(int) const;       // Print a specific column    new line
 
     void printIntersection(int, int) const;
     void printIntersections()        const;
@@ -43,7 +71,19 @@ public:
     void setValuesDiagonal_UpperLeftToBottomRight(const Element &);
     void setValuesDiagonal_UpperRightToBottomLeft(const Element &);
 
-    inline int getNumRows() const {return numRows;};
-    inline int getNumCols() const {return numCols;};
+    inline int      getNumRows()  const {return numRows;};
+    inline int      getNumCols()  const {return numCols;};
+    inline Position getPosition() const {return position;};
+    inline void     setPosition(  const Position & pos) {
+                            this->position.setX(pos.getX());
+                            this->position.setY(pos.getY());
+                            this->position.setZ(pos.getZ());};
 
+    inline List *getRows()    const {return rows;};
+    inline List *getColumns() const {return columns;};
+
+    inline const Matrix * getNext() const{return next;};
+    inline       Matrix * getNext()      {return next;};
+    inline       Matrix * setNext(Matrix * direction)         // Setter for the next Position in the linked list
+                    {this->next = direction;return this;};
 };
