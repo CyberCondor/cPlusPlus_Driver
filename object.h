f3c6f40013f7346d9ed3eb242711e4b708b832d6
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-10-29 16:29:23 -0700

    Update object.h

--- object.h
+++ object.h
@@ -1,22 +1,63 @@
 #pragma once
 #include <iostream>
 #include "element.h"
+#include "string.h"
 #include "matrix.h"
+#include "space.h"
+#include "position.h"
+#include "property.h"
+#include "properties.h"
 
 class Object {
 private:
-    Matrix matrix; // Matrix to represent the object
-    Element name;  // Name of the object
-
+    Element    name;        // Name of the Object
+    Space      space;       // 3D Space to represent the Object.
+    Matrix     matrix;      // 2D Matrix to represent the Object.
+    Properties properties;  // Properties to define properties of the Object
+    Position   position;    // Position to locate the Object in Space.
 public:
-    Object();
-    Object(Element, int, int);           // Param Constructor
+    inline Object() : name("NULL"), space(), matrix(), properties(), position(){};
+
+    inline Object(const Element & name)                           : name(name),   // Param Constructor
+                                                                    space(name),
+                                                                    matrix(3,3,name), 
+                                                                    properties(), 
+                                                                    position()
+                                                                   {this->properties.insert(Element("Name"),Element(name));};
+    
+    inline Object(const Element & name, int numRows, int numCols) : name(name),   // Param Constructor
+                                                                    space(numRows, numCols, name), 
+                                                                    matrix(numRows, numCols, name), 
+                                                                    properties(),
+                                                                    position()
+                                                                   {this->properties.insert(Element("Name"),Element(name));};
 
     void rename(const Element &);
 
     void print() const ;
 
-    void setMatrixValue(int row, int col, const Element &);
+    inline void setPosition(const Position & pos) {
+                            Element buffer = Element("(");
+                            buffer += pos.getX();
+                            buffer += Element(",");
+                            buffer += pos.getY();
+                            buffer += Element(")->");
+                            buffer += pos.getZ();
+                            this->properties.insert(Element("Position"),buffer);
+                            this->position.setX(pos.getX());
+                            this->position.setY(pos.getY());
+                            this->position.setZ(pos.getZ());};
+
+    void setDefaultProperties();
+    bool addProperty(const Property &);
+    bool addProperty(const Element  &);
+    bool addProperty(const Element  &, const Element &);
+    bool addProperty(const Element  &, const String  &);
+    bool setProperty(const Property &);
+    bool setProperty(const Element  &, const Element &);
+    bool setProperty(const Element  &, const String  &);
+
+    void setMatrixValue (int row, int col, const Element &);
     void setMatrixValues(const Element &);
 
     void setAllMatrixValues_UpperLeftToBottomRight(const Element &);
@@ -32,6 +73,12 @@ public:
     inline int  getNumRows() const {return matrix.getNumRows();};
     inline int  getNumCols() const {return matrix.getNumCols();};
     
-    inline Matrix &getMatrix()     {return matrix;};
-    inline Element &getName()      {return name;};
+    inline Element    const getName()       const {return name;      };
+    inline Matrix     const getMatrix()     const {return matrix;    };
+    inline Position   const getPosition()   const {return position;  };
+    inline Properties const getProperties() const {return properties;};
+    inline Element         &getName()             {return name;      };
+    inline Matrix          &getMatrix()           {return matrix;    };
+    inline Position        &getPosition()         {return position;  };
+    inline Properties      &getProperties()       {return properties;};
 };
