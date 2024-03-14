92afe68cc5acac5dddbd2f0f0e9498ef831c4219
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2024-03-14 13:26:11 -0700

    Update properties.h
    Init search functions, getters, and setters

--- properties.h
+++ properties.h
@@ -13,6 +13,7 @@ public:
     Properties() : head(nullptr) {};
     Properties(Property*);
     Properties(Property);
+    Properties(const Properties &);
 
     void insert(Property *, int);
     bool insert(Property *);
@@ -21,9 +22,30 @@ public:
     bool insert(const Element  &, const Element &);
     bool insert(const Element  &, const String  &);
 
+    bool       findMatch(const Element &,const Element &);   // Find Match Key to Value
+    Properties findMatch          (const Element &);         // Find Exact Match
+    Properties findMatch_i        (const Element &);         // Find Match                  - CASE INSENSITIVE
+    Properties containsSequence   (const Element &);         // Contains Chars in Sequence
+    Properties containsSequence_i (const Element &);         // Contains                    - CASE INSENSITIVE
+    Properties wildcardSearch     (const Element &);         // Search String Starts with*, ends *with, or *contains*
+    Properties wildcardSearch_i   (const Element &);         // Search String Starts with*, ends *with, or *contains* - CASE INSENSITIVE
+
+    Properties getKeys() const;
+    Properties getProperties() const;
+
     void print() const;
 
     int size() const; 
 
     void setDefault();
+   
+    void clear();
+    
+
+    inline const Property * getHead()    const {return head;};         // Get Head
+    inline       Property * getHead()          {return head;};         // Get Head
+    inline       Property * setHead()    const {return head;};         // Set Head  
+
+    Properties   operator+ (const Properties  &other) const;           // Addition operator
+    Properties & operator= (const Properties  &other);                 // Copy assignment operator
 };
