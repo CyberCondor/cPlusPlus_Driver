cb0951fca4cb0f2ac9f6c5fff9ef64635bdf4fed
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2024-03-14 13:48:11 -0700

    Update main.h
    Add objects.h

--- main.h
+++ main.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 #include "element.h"
 #include "string.h"
 #include "list.h"
 #include "matrix.h"
 #include "space.h"
 #include "object.h"
+#include "objects.h"
 #include "position.h"
 #include "positions.h"
 #include "property.h"
