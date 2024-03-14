7c30d8fa2c5793122269998a77e05ac482777dbb
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2024-03-14 14:00:44 -0700

    Import CSV Files into Objects
    This main looks for csv files in the runtime arguments. For csv files found in argument list, it imports the csv as a matrix object into a circular linked list of objects. Correlations are made between the csv file data property values and all other csv files. Then this main displays objects identified during object import based on the data in the csv files found in arguments during runtime.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,18 @@
 #include "main.h"
 
-/*
-While the C++ language standard provides minimum size requirements for data types, 
-the exact size of data types is determined by a combination of factors, 
-including the compiler, target hardware, compiler flags, and platform-specific considerations. 
-*/
+int main(int argc, char **argv, char **envp)
+{
+    Objects CSVs;                           // Init Main Object of Objects to hold information from CSVs
 
-int main(int argc, char **argv, char **envp) {
+    List list;                              // Init list to hold argument values
+    for(auto idx = 0; idx < argc; idx++)    // For the Argument Count 
+    {                                       // Put the parameterized arguments into an organized list
+        Element buffer(argv[idx]);          // Using the Element Constructor to copy argv into Element
+        list.insert(buffer);                // And the Sorted Insertion Mutator to insert the Element into the list
+    }
 
-    Object object(Element("Pie"), 4,3);
-
-    object.setDefaultProperties();
-
-    object.print();
-
-    object.rename(Element("Booooooooooooo!"));
-
-    object.addProperty(Element("Length"),        String(Element("9 lawn gnomes")));
-    object.addProperty(Element("Wanted Level"),  Element("6 stars"));
-    object.addProperty(Element("Freedom Points"),Element("Wanted"));
-    object.addProperty(Element("longness"),      Element("6 longlongs"));
-    object.addProperty(Element("Status"),        Element("On the run"));
-    object.setProperty(Element("Description"),   Element("Not a Ghost"));
-
-    object.setPosition(Position(0,0,1));
-
-    object.print();
-
-    object.getMatrix().transpose();
-
-    object.print();
+    CSVs.importCSVs(list);                  // Import CSVs into objects
+    CSVs.print();                           // Display the objects
 
     return 0;
 }
