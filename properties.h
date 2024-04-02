5d9412c35b81e83e1be30abff0751829b7d577f5
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2024-04-02 09:29:02 -0700

    Update properties.h
    Expand search functions

--- properties.h
+++ properties.h
@@ -22,16 +22,25 @@ public:
     bool insert(const Element  &, const Element &);
     bool insert(const Element  &, const String  &);
 
-    bool       findMatch(const Element &,const Element &);   // Find Match Key to Value
-    Properties findMatch          (const Element &);         // Find Exact Match
-    Properties findMatch_i        (const Element &);         // Find Match                  - CASE INSENSITIVE
-    Properties containsSequence   (const Element &);         // Contains Chars in Sequence
-    Properties containsSequence_i (const Element &);         // Contains                    - CASE INSENSITIVE
-    Properties wildcardSearch     (const Element &);         // Search String Starts with*, ends *with, or *contains*
-    Properties wildcardSearch_i   (const Element &);         // Search String Starts with*, ends *with, or *contains* - CASE INSENSITIVE
+    bool       findMatch         (const Element &, const Element &);// Find Match Key to Value
+    bool       findMatch_i       (const Element &, const Element &);// Find Match Key to Value
+    Properties findMatch         (const Element &);                 // Find Exact Match
+    Properties findMatch_i       (const Element &);                 // Find Match                  - CASE INSENSITIVE
+    bool       containsSequence  (const Element &, const Element &);// Key Contains Value
+    bool       containsSequence_i(const Element &, const Element &);// Key Contains Value
+    Properties containsSequence  (const Element &);                 // Contains Chars in Sequence
+    Properties containsSequence_i(const Element &);                 // Contains                    - CASE INSENSITIVE
+    bool       wildcardSearch    (const Element &, const Element &);// Key like *value*
+    bool       wildcardSearch_i  (const Element &, const Element &);// Key like *value*
+    Properties wildcardSearch    (const Element &);             // Search String Starts with*, ends *with, or *contains*
+    Properties wildcardSearch_i  (const Element &);             // Search String Starts with*, ends *with, or *contains* - CASE INSENSITIVE
 
+
+    bool       findKey(const Element &) const;
     Properties getKeys() const;
     Properties getProperties() const;
+    Property   getProperty(const Element & key) const;
+    Property   getProperty(int pos) const;
 
     void print() const;
 
